1fc9588b1cc2006021d48678ca8578ec693ba992
Karolis Labrencis <karolis@labrencis.lt>
2016-11-12 10:39:06 +0200

    Add fn for generating Auth object

--- src/pac/core.clj
+++ src/pac/core.clj
@@ -1,4 +1,30 @@
-(ns pac.core)
+(ns pac.core
+  (:import (java.security MessageDigest)))
+
+(defn sha1-str [s]
+  (->> (-> "sha1"
+           MessageDigest/getInstance
+           (.digest (.getBytes s "UTF-8")))
+       (map #(.substring
+              (Integer/toString
+                (+ (bit-and % 0xff) 0x100) 16) 1))
+       (apply str)))
+
+(defn generate-auth [secret payload]
+  (-> (str payload secret)
+      (sha1-str)
+      (clojure.string/lower-case)))
+
+(defn auth [team client session secret sequence]
+  {:TeamName       team
+   :ClientName     client
+   :SessionId      session
+   :SequenceNumber sequence
+   :AuthCode       (generate-auth
+                     secret
+                     (str team ":" client ":" session ":" sequence))})
+
+
 
 (defn foo
   "I don't do a whole lot."

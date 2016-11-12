97cadd00bc5edd88110ba208f44ba8c28b0d3102
Osvaldas Grigas <osvaldas@inventi.lt>
2016-11-12 14:11:06 +0200

    protocol: return previous positions

--- src/pac/protocol.clj
+++ src/pac/protocol.clj
@@ -24,7 +24,8 @@
 (defn to-position-map [[y x]]
   {:Row y :Col x})
 
-(defn game-map [{:keys [Mode Map TecmanPosition GhostPositions]}]
+(defn game-map
+  [{:keys [Mode Map TecmanPosition PreviousTecmanPosition GhostPositions PreviousGhostPositions]}]
   (let [{:keys [Width Height Rows]} Map
         cell-by-char {\# {:type :wall} \. {:type :cookie}}
         rows (->> Rows (map vec) (mapv #(mapv cell-by-char %)))
@@ -40,7 +41,9 @@
                           (assoc-in (ghost-poses 2) {:type :ghost})
                           (assoc-in (ghost-poses 3) {:type :ghost}))
      :tecman-position tecman-pos
-     :ghost-positions ghost-poses}))
+     :tecman-previous-position (to-position-vector PreviousTecmanPosition)
+     :ghost-positions ghost-poses
+     :ghost-previous-positions (mapv to-position-vector PreviousGhostPositions)}))
 
 (defn positions-req [position-vectors]
   {:Positions (map to-position-map position-vectors)})

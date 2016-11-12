9bd64e252b76669bb21d9fc98d8a1a02a27c149a
Zilvinas Kybartas <zilvinas@inventi.lt>
2016-11-12 13:35:27 +0200

    extract auth stuff

---
+++ src/pac/auth.clj
@@ -0,0 +1,49 @@
+(ns pac.auth
+  (:require [org.httpkit.client :as http]
+            [cheshire.core :as cheshire]
+            [pac.protocol :as protocol])
+  (:import (java.security MessageDigest)))
+
+(defn sha1-str [s]
+  (->> (-> "sha1"
+           MessageDigest/getInstance
+           (.digest (.getBytes s "UTF-8")))
+       (map #(.substring
+              (Integer/toString
+                (+ (bit-and % 0xff) 0x100) 16) 1))
+       (apply str)))
+
+(defn generate-auth [secret payload]
+  (-> (str payload secret)
+      (sha1-str)
+      (clojure.string/lower-case)))
+
+(defn auth [team session secret client sequence]
+  {:TeamName       team
+   :ClientName     client
+   :SessionId      session
+   :SequenceNumber sequence
+   :AuthCode       (generate-auth
+                     secret
+                     (str team ":" client ":" session ":" sequence))})
+
+(def dev-url "http://10.30.0.114/dev/ClientService.svc")
+(def dev-pwd "gkOOdYu9")
+
+(def prod-url "http://10.30.0.114/prod/ClientService.svc")
+(def prod-pwd "DRI9idEoD91j")
+
+(defonce sq (atom 1))
+
+(defn post-params [player session body]
+  (swap! sq inc)
+  {:body
+   (cheshire/generate-string
+     (merge body
+            {:Auth (auth "Inventi" session dev-pwd player @sq)}))
+   :headers {"Content-Type" "application/json"}})
+
+(defn post [player session path body]
+  (http/post
+    (str dev-url (str "/json/" path))
+    (post-params player session body)))
--- src/pac/core.clj
+++ src/pac/core.clj
@@ -1,61 +1,16 @@
 (ns pac.core
   (:require [org.httpkit.client :as http]
             [cheshire.core :as cheshire]
-            [pac.protocol :as protocol])
+            [pac.protocol :as protocol]
+            [pac.auth :refer [post]])
   (:import (java.security MessageDigest)))
-
-(def dev-url "http://10.30.0.114/dev/ClientService.svc")
-(def dev-pwd "gkOOdYu9")
-
-(def prod-url "http://10.30.0.114/prod/ClientService.svc")
-(def prod-pwd "DRI9idEoD91j")
-
-(defn sha1-str [s]
-  (->> (-> "sha1"
-           MessageDigest/getInstance
-           (.digest (.getBytes s "UTF-8")))
-       (map #(.substring
-              (Integer/toString
-                (+ (bit-and % 0xff) 0x100) 16) 1))
-       (apply str)))
-
-(defn generate-auth [secret payload]
-  (-> (str payload secret)
-      (sha1-str)
-      (clojure.string/lower-case)))
-
-(defn auth [team session secret client sequence]
-  {:TeamName       team
-   :ClientName     client
-   :SessionId      session
-   :SequenceNumber sequence
-   :AuthCode       (generate-auth
-                     secret
-                     (str team ":" client ":" session ":" sequence))})
-
-(defonce sq (atom 1))
-
-(defn post-params [player session body]
-  (swap! sq inc)
-  {:body
-   (cheshire/generate-string
-     (merge body
-            {:Auth (auth "Inventi" session dev-pwd player @sq)}))
-   :headers {"Content-Type" "application/json"}})
-
-(defn post [player session path body]
-  (http/post
-    (str dev-url (str "/json/" path))
-    (post-params player session body)))
 
 (defn create-player [player session]
   "Registers a player. Returns player id which
   should be used in follow-up requests"
-  (println "create" auth)
   (let [response (post player session "CreatePlayer" {})
         body (:body @response)]
     (cheshire/parse-string body keyword)))
-
 
 (defonce turn (atom 0))
 
@@ -84,7 +39,6 @@
     (println body)
     (protocol/status-ok? body)
     nil))
-
 
 (defn do-move [player session player-id coords]
   (wait-next-turn player session player-id)

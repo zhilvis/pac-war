d5869f140bf1e5c3acce67f8a792b82b69391c52
Karolis Labrencis <karolis@labrencis.lt>
2016-11-12 11:09:34 +0200

    Add create player

--- src/pac/core.clj
+++ src/pac/core.clj
@@ -1,5 +1,9 @@
 (ns pac.core
+  (:require [org.httpkit.client :as http]
+            [cheshire.core :as cheshire])
   (:import (java.security MessageDigest)))
+
+(def base-url "http://10.30.0.114/dev/ClientService.svc")
 
 (defn sha1-str [s]
   (->> (-> "sha1"
@@ -15,7 +19,7 @@
       (sha1-str)
       (clojure.string/lower-case)))
 
-(defn auth [team client session secret sequence]
+(defn auth [team session secret client sequence]
   {:TeamName       team
    :ClientName     client
    :SessionId      session
@@ -24,7 +28,17 @@
                      secret
                      (str team ":" client ":" session ":" sequence))})
 
+(def inventi-dev (partial auth "Inventi" 5658 "Karolis" "gkOOdYu9"))
 
+(defn create-player [auth]
+  "Registers a player. Returns player id which
+  should be used in follow-up requests"
+  (let [response (http/post
+                   (str base-url "/json/CreatePlayer")
+                   {:body    (cheshire/generate-string {:Auth (auth 1)})
+                    :headers {"Content-Type" "application/json"}})
+        body (:body @response)]
+    (cheshire/parse-string body)))
 
 (defn foo
   "I don't do a whole lot."

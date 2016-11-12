453d8d85989baa03a411366a4c1ebd761a914577
Zilvinas Kybartas <zilvinas@inventi.lt>
2016-11-12 09:49:23 +0200

    initial commit

---
+++ src/pac/core.clj
@@ -0,0 +1,6 @@
+(ns pac.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/pac_war/core_test.clj
@@ -0,0 +1,7 @@
+(ns pac-war.core-test
+  (:require [clojure.test :refer :all]
+            [pac-war.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

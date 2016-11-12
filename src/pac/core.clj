bdeed5b38c7f95b9e4faa1db7f64915b09e6d7c4
Karolis Labrencis <karolis@labrencis.lt>
2016-11-12 13:58:39 +0200

    Implement main loop

--- src/pac/core.clj
+++ src/pac/core.clj
@@ -10,7 +10,7 @@
   should be used in follow-up requests"
   (let [response (post player session "CreatePlayer" {})
         body (:body @response)]
-    (cheshire/parse-string body keyword)))
+    (:PlayerId (cheshire/parse-string body keyword))))
 
 (defonce turn (atom 0))
 
@@ -19,15 +19,15 @@
     (let [response (post player session "WaitNextTurn" {:PlayerId player-id
                                                         :RefTurn  @turn})
           body (cheshire/parse-string (:body @response) keyword)]
-      (println body)
+      (println "WaitNextTurn" body)
       (if-not (:TurnComplete body)
         (recur)
-        true))))
+        (not (:GameFinished body))))))
 
 (defn get-player-view [player session player-id]
   (let [response (post player session "GetPlayerView" {:PlayerId player-id})
         body (cheshire/parse-string (:body @response) keyword)]
-    (println body)
+    (println "GetPlayerView" body)
     (when (protocol/status-ok? body)
       (do (reset! turn (protocol/turn body))
           (protocol/game-map body)))))
@@ -36,11 +36,22 @@
   (let [response (post player session "PerformMove" (merge {:PlayerId player-id}
                                                            (protocol/positions-req coords)))
         body (cheshire/parse-string (:body @response) keyword)]
-    (println body)
+    (println "PerformMove" body)
     (protocol/status-ok? body)
     nil))
 
-(defn do-move [player session player-id coords]
-  (wait-next-turn player session player-id)
-  (println (get-player-view player session player-id))
-  (perform-move player session player-id coords))
+(defn play-until-completes [player-name session-id player-id]
+  (loop []
+    (when (wait-next-turn player-name session-id player-id)
+      (when-let [view (get-player-view player-name session-id player-id)]
+        (perform-move player-name session-id player-id (ai/external-advice-turn view)))
+      (recur))))
+
+(defn -main []
+  (let [session-id (int (* 10000 (Math/random)))
+        player-name (str "Inventi-" session-id)
+        player-id (create-player player-name session-id)]
+    (loop []
+      (reset! turn 0)
+      (play-until-completes player-name session-id player-id)
+      (recur))))
